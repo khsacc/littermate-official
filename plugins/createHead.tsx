30cf6826d700d8ebf8a0236daed1874d3fe5710a
khsacc <hero.key629522@gmail.com>
2021-06-14 23:51:35 +0900

    modified createHead plugin

--- plugins/createHead.tsx
+++ plugins/createHead.tsx
@@ -1,15 +1,64 @@
 import { NextPage } from "next";
 import Head from "next/head";
 
+type TwitterCard = "summary" | "summary_large_image" | "app" | "player";
+
+const defaultValues = {
+  image: "",
+  description: "",
+  twitterCard: "summary_large_image" as TwitterCard,
+};
+
 const CreateHead: NextPage<{
   title?: string;
   image?: string;
   description?: string;
-}> = ({ title, image, description }) => {
+  twitterCard?: TwitterCard;
+}> = ({
+  title,
+  image = defaultValues.image,
+  description = defaultValues.description,
+  twitterCard = defaultValues.twitterCard,
+}) => {
   return (
     <Head>
-      <title>{title || "Littermate"}</title>
-      <meta></meta>
+      <title>
+        {title
+          ? `${title} | Littermate Official Website`
+          : "Littermate Official Website"}
+      </title>
+      {[
+        {
+          property: "og:title",
+          content: title,
+        },
+        {
+          property: "og:description",
+          content: description,
+        },
+        {
+          property: "og:image",
+          content: image,
+        },
+        {
+          property: "og:type",
+          content: "website",
+        },
+        {
+          property: "og:locale",
+          content: "ja_JP",
+        },
+      ].map((e) => (
+        <meta property={e.property} content={e.content}></meta>
+      ))}
+      {[
+        {
+          name: "twitter:card",
+          content: twitterCard,
+        },
+      ].map((e) => (
+        <meta name={e.name} content={e.content}></meta>
+      ))}
     </Head>
   );
 };
