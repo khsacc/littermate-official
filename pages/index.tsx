d39524d84eb248178d73d02bebdcff7cc7a9d0ef
khsacc <hero.key629522@gmail.com>
2021-06-15 00:20:24 +0900

    added logo component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import { NextPage } from "next";
+import { LittermateLogo } from "../components/Logo";
 
 const Home: NextPage = () => {
-  return <div>test</div>;
+  return (
+    <div>
+      <LittermateLogo />
+    </div>
+  );
 };
 
 export default Home;
