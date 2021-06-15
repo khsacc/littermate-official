d41f9da2de0d51c02c01da0f112885df57767d90
khsacc <hero.key629522@gmail.com>
2021-06-15 22:02:11 +0900

    setup google analytics tracking

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheets } from "@material-ui/core/styles";
 import { Theme } from "../styles/theme";
+import { GA_ID, isGaIdSet } from "../plugins/gtag";
 
 export default class MyDocument extends Document {
   render() {
@@ -10,16 +11,35 @@ export default class MyDocument extends Document {
         <Head>
           {/* PWA primary color */}
           <meta name="theme-color" content={Theme.palette.primary.main} />
+          {/* Roboto font (default font for material-ui) */}
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
           {/* todo: add meta tags */}
+
           {/* Adobe Fonts */}
-          <link
-            rel="stylesheet"
-            href="https://use.typekit.net/vpq5jbc.css"
-          ></link>
+          <link rel="stylesheet" href="https://use.typekit.net/vpq5jbc.css" />
+          {/* Google Analytics */}
+          {isGaIdSet && (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+              />
+              <script
+                async
+                dangerouslySetInnerHTML={{
+                  __html: `window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${GA_ID}', {
+                      page_path: window.location.pathname,
+                    });`,
+                }}
+              />
+            </>
+          )}
         </Head>
         <body>
           <Main />
