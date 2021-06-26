ed30f52ba068c7115cca52cf7593d9ac67aa7f52
khsacc <hiroki@eqchem.s.u-tokyo.ac.jp>
2021-06-27 00:54:42 +0900

    added sequence

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import Head from "next/head";
-import { ThemeProvider } from "@material-ui/core/styles";
+import { makeStyles, ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { Theme } from "../styles/theme";
 import { Header } from "../components/Header";
@@ -9,7 +9,28 @@ import { Footer } from "../components/Footer";
 import router from "next/router";
 import * as gtag from "../plugins/gtag";
 
+const useStyles = makeStyles(() => {
+  return {
+    wholeWrapper: {
+      position: "relative",
+      height: "fit-content",
+    },
+    sequence: {
+      position: "absolute",
+      top: 0,
+      right: 0,
+      height: "100%",
+      width: "50%",
+      backgroundImage: "url('/image/sequence/sequence.svg')",
+    },
+    pageWrapper: {
+      minHeight: "100vh",
+    },
+  };
+});
+
 export default function MyApp(props) {
+  const classes = useStyles(Theme);
   const { Component, pageProps } = props;
 
   // Remove the server-side injected CSS to avoid hydration errors.
@@ -48,7 +69,13 @@ export default function MyApp(props) {
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Header />
-        <Component {...pageProps} />
+        <div className={classes.wholeWrapper}>
+          <div className={classes.sequence}></div>
+          <div className={classes.pageWrapper}>
+            <Component {...pageProps} />
+          </div>
+        </div>
+
         <Footer />
       </ThemeProvider>
     </React.Fragment>
