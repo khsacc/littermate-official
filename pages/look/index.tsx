5f68736f14e0d1c06e8bdfba1c81a7bbfb2f93e8
khsacc <hiroki@eqchem.s.u-tokyo.ac.jp>
2021-07-05 21:25:06 +0900

    added look index

--- pages/look/index.tsx
+++ pages/look/index.tsx
@@ -1,10 +1,26 @@
+import { makeStyles, Typography } from "@material-ui/core";
 import { NextPage } from "next";
 import { CreateHead } from "../../plugins/createHead";
+import { Theme } from "../../styles/theme";
+
+const useStyles = makeStyles((theme) => {
+  return {
+    pagetitle: {
+      textAlign: "center",
+    },
+  };
+});
 
 const LookIndex: NextPage = () => {
+  const classes = useStyles(Theme);
   return (
     <>
       <CreateHead title="Look" />
+      <main>
+        <Typography variant="h2" className={classes.pagetitle}>
+          Look
+        </Typography>
+      </main>
     </>
   );
 };
