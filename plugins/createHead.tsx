3e1007cd2a1ce942ed9ee845de5cc88fb9af5e78
khsacc <hiroki@eqchem.s.u-tokyo.ac.jp>
2021-08-04 16:52:26 +0900

    added favicon

--- plugins/createHead.tsx
+++ plugins/createHead.tsx
@@ -59,6 +59,31 @@ export const CreateHead: NextPage<{
       ].map((e, i) => (
         <meta name={e.name} content={e.content} key={i}></meta>
       ))}
+      {/* // favicon */}
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/image/meta/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/image/meta/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/image/meta/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/image/meta/site.webmanifest" />
+      <link
+        rel="mask-icon"
+        href="/image/meta/safari-pinned-tab.svg"
+        color="#5bbad5"
+      />
+      <meta name="msapplication-TileColor" content="#2b5797"></meta>
     </Head>
   );
 };
