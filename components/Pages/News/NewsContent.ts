116b6562e6e377a2f5a07f4e3acf0f0149f58a41
khsacc <hiroki@eqchem.s.u-tokyo.ac.jp>
2021-11-03 15:46:13 +0900

    added news

--- components/Pages/News/NewsContent.ts
+++ components/Pages/News/NewsContent.ts
@@ -1,9 +1,14 @@
 import { NextPage } from "next";
+import { HatchPopupNews } from "./content/2021-hatch-popup";
 import { PopupNews } from "./content/2021-summer-popup";
 
 export const NewsContents: { id: string; content: NextPage }[] = [
   {
     id: "2021-summer-popup",
     content: PopupNews,
+  },
+  {
+    id: "2021-hatch-popup",
+    content: HatchPopupNews,
   },
 ];
