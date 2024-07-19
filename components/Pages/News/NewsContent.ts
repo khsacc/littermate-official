a0a00159d5a6c9d491bda8741e4dfdc76143e1fb
増田雄一朗 <opisthocoericaudia@gmail.com>
2024-07-19 23:56:31 +0900

    ニュースを更新

--- components/Pages/News/NewsContent.ts
+++ components/Pages/News/NewsContent.ts
@@ -2,6 +2,7 @@ import { NextPage } from "next";
 import { SpringPopupNews } from "./content/2022-spring-popup";
 import { HatchPopupNews } from "./content/2021-hatch-popup";
 import { PopupNews } from "./content/2021-summer-popup";
+import { CognicodeNews } from "./content/2023-cognicode";
 
 export const NewsContents: { id: string; content: NextPage }[] = [
   {
@@ -15,5 +16,9 @@ export const NewsContents: { id: string; content: NextPage }[] = [
   {
     id: "2022-spring-popup",
     content: SpringPopupNews,
+  },
+  {
+    id: "2023-cognicode",
+    content: CognicodeNews,
   },
 ];
